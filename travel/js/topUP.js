cd14d19dd1c3164f76e567baa98dc44c6578a6d2
kavin-lee <kavin_lee@yeah.net>
2019-06-20 09:02:44 +0800

    ggg

--- travel/js/topUP.js
+++ travel/js/topUP.js
@@ -1,4 +1,9 @@
 $(function(){
+    $(".choice .other input").click(function () {
+        console.log('..')
+        $(this).parents($('.choice')).children($('.input1')).attr('checked', false)
+    })
+    
     $("#zhifubao").click(function(){
         $("#main form").css("margin-right","611px")
         $(".imger").attr("src","../img/footer/zhifubao.jpg").css("display","block")
